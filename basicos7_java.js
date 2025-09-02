02694fc8bd641302afcdbcd0fb6c10ff33ce3e6c
d3lac0baa <sercoga2006@gmail.com>
2025-09-03 00:55:25 +0200

    upload

---
+++ basicos7_java.js
@@ -0,0 +1,11 @@
+function greaterNumber(numberOne , numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  } else {
+    console.log(numberTwo);
+  }
+}
+
+greaterNumber(10, 5);
+greaterNumber(3, 8);
+greaterNumber(7, 7);
